a8ae84bbaf4d4db17200672d9dae281934b6b728
Gigman2 <86406672+Gigman2@users.noreply.github.com>
2023-02-01 05:06:36 +0000

    some update

--- src/components/Blocks/Header/header2.tsx
+++ src/components/Blocks/Header/header2.tsx
@@ -5,10 +5,9 @@ const PageHeader:FC<{title: string, children?: JSX.Element}> = ({title, children
   // if user is online, return the child component else return a custom component
 
   return (
-     <Flex justify="space-between" w="100%" color="kc.500" borderBottomWidth={2} borderColor="kc.500" pb={6}>
-        <Text textTransform={"uppercase"} letterSpacing={-0.8} fontSize={14} fontWeight={600}>{title}</Text>
-
-       {children}
+    <Flex align={"center"} justify="space-between" w="100%" color="kc.500" borderBottomWidth={2} borderColor="kc.500" pb={6}>
+        <Text textTransform={"uppercase"} fontSize={14} fontWeight={600}>{title}</Text>
+        {children}
     </Flex>
   );
 };
--- src/pages/admin/components/home/index.tsx
+++ src/pages/admin/components/home/index.tsx
@@ -1,8 +1,8 @@
 import {useEffect} from 'react'
-import { Box, Flex, Grid, GridItem, Text } from "@chakra-ui/react";
-import { CustomButton } from "components/Atoms";
+import { Box, Flex, Input, Text } from "@chakra-ui/react";
 import PageHeader from "components/Blocks/Header/header2";
 import useLayout from "stores/layout";
+import { CustomButton } from 'components/Atoms';
 
 function Home() {
     const { setDashboard } = useLayout(['setDashboard'])
@@ -15,7 +15,28 @@ function Home() {
 
     return (
     <Box>
-        <PageHeader title='Reports'></PageHeader>
+        <PageHeader title='Reports'>
+            <Flex color="kc.500" align={"center"}>
+                <Text fontWeight={600} fontSize={14} mr={6}>Filter by Date</Text>
+                <Flex columnGap={4}>
+                    <Box>
+                        <Input color="kc.500" type={"date"} w={52} borderWidth={1} borderColor={"kc.500"}/>
+                    </Box>
+                    <Box>
+                        <Input color="kc.500" type={"date"} w={52} borderWidth={1} borderColor={"kc.500"}/>
+                    </Box>
+                    <Box>
+                        <CustomButton title="Generate new report" 
+                            color="kc" btype='solid' 
+                            py={1}
+                            h={10}
+                            fontSize={14} 
+                            fontWeight={500}
+                        />
+                    </Box>
+                </Flex>
+            </Flex>
+        </PageHeader>
     </Box>
     )
 }
